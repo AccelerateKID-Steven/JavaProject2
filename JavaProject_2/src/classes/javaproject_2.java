b890bc8aa848b8420550c9c9cff9b1e061eb6d90
Nelikohai <69607761+Nelikohai@users.noreply.github.com>
2021-04-13 15:57:47 -0400

    Delete javaproject_2.java

--- JavaProject_2/src/classes/javaproject_2.java
+++
@@ -1,4 +0,0 @@
-package classes;
-//<<<<<<< Upstream, based on branch 'main' of https://github.com/AccelerateKID-Steven/JavaProject2.git
-//This Project is made by: Cocodadog, Potaioss, BlastedGlass07
-//=======
