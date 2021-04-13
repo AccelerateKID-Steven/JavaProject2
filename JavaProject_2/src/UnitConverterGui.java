8ca96567965be3dcd2f539a856dba312faafe4e0
Nelikohai <69607761+Nelikohai@users.noreply.github.com>
2021-04-13 16:27:45 -0400

    Add files via upload

---
+++ JavaProject_2/src/UnitConverterGui.java
@@ -0,0 +1,84 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JSplitPane;
+import java.awt.Insets;
+import javax.swing.JSeparator;
+import java.awt.Color;
+
+public class UnitConverterGui {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UnitConverterGui window = new UnitConverterGui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UnitConverterGui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 620, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblTitle = new JLabel("Imperial/Metric Item Converter");
+		lblTitle.setForeground(Color.WHITE);
+		lblTitle.setBounds(0, 0, 606, 39);
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setFont(new Font("Microsoft JhengHei UI Light", Font.BOLD, 20));
+		frame.getContentPane().add(lblTitle);
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		separator.setForeground(Color.WHITE);
+		separator.setBounds(301, 37, 2, 203);
+		frame.getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setForeground(Color.WHITE);
+		separator_1.setBounds(10, 37, 586, 2);
+		frame.getContentPane().add(separator_1);
+		
+		JLabel lblequation = new JLabel("=");
+		lblequation.setBackground(Color.DARK_GRAY);
+		lblequation.setHorizontalAlignment(SwingConstants.CENTER);
+		lblequation.setForeground(Color.WHITE);
+		lblequation.setFont(new Font("Arial", Font.BOLD, 30));
+		lblequation.setBounds(280, 250, 45, 13);
+		frame.getContentPane().add(lblequation);
+		
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setOrientation(SwingConstants.VERTICAL);
+		separator_2.setForeground(Color.WHITE);
+		separator_2.setBounds(301, 273, 2, 280);
+		frame.getContentPane().add(separator_2);
+	}
+}
