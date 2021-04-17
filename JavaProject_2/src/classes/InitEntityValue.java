c03520fc448b3726f9922e18d958724dc4c144c2
Nelikohai <69607761+Nelikohai@users.noreply.github.com>
2021-04-17 18:01:05 -0400

    Delete JavaProject_2/src/classes directory

--- JavaProject_2/src/classes/InitEntityValue.java
+++
@@ -1,9 +0,0 @@
-package classes;
-//<<<<<<< Upstream, based on branch 'main' of https://github.com/AccelerateKID-Steven/JavaProject2.git
-//This Project is made by: Cocodadog, Potaioss, BlastedGlass07
-//Metric System Calculator/Converter - has item, converts measurements of it into metric or just convert
-
-public class InitEntityValue
-{
-	
-}
